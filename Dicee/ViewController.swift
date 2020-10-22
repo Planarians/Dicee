560eab5b36f28a6af173914f1f16538d15f0723b
“planarians” <lyplanarians@gmail.com>
2020-10-22 20:37:23 +0800

    14.常量变量以及基础类型

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var index1 : Int = 0
+    var index2 : Int = 0
+    
+
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+
+    
+    @IBAction func rollButton(_ sender: Any) {
+        index1 = Int.random(in: 1...6)
+        index2 = Int.random(in: 1...6)
+        
+        print(index1)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
