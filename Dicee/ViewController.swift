dc074187a2a88b2bcc6008f1ce8e79b99435f8d3
“planarians” <lyplanarians@gmail.com>
2020-10-23 12:09:46 +0800

    15

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6",]
+    
+
 
     var index1 : Int = 0
     var index2 : Int = 0
@@ -21,10 +25,15 @@ class ViewController: UIViewController {
 
     
     @IBAction func rollButton(_ sender: Any) {
-        index1 = Int.random(in: 1...6)
-        index2 = Int.random(in: 1...6)
+        index1 = Int.random(in: 0...5)
+        index2 = Int.random(in: 0...5)
         
-        print(index1)
+        
+        
+        diceImageView1.image = UIImage(named: diceArray[index1])
+        
+        diceImageView2.image = UIImage(named: diceArray[index2])
+//        print(index1)
     }
     
     
